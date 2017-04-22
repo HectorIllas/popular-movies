658d2da27ec3129f10acb2d4d4afa4ce9052ac8c
Hector <karkinos505@gmail.com>
2017-04-21 22:03:04 -0400

    MVP update

--- app/src/main/java/com/hector/movieapp/movies/MoviesPresenter.java
+++ app/src/main/java/com/hector/movieapp/movies/MoviesPresenter.java
@@ -3,6 +3,9 @@ package com.hector.movieapp.movies;
 import android.support.annotation.NonNull;
 
 import com.hector.movieapp.data.Movie;
+import com.hector.movieapp.data.source.MoviesRepository;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Created by hector on 4/15/17.
@@ -10,13 +13,21 @@ import com.hector.movieapp.data.Movie;
 
 public class MoviesPresenter implements MoviesContract.Presenter {
 
-    //private final MoviesContract.View moviesView;
+    private final MoviesRepository moviesRepository;
+    private final MoviesContract.View moviesView;
+    private boolean firstLoad  = true;
 
+    public MoviesPresenter(@NonNull MoviesRepository moviesRepository, @NonNull MoviesContract.View moviesView) {
+        this.moviesRepository = checkNotNull(moviesRepository, "moviesRepository cannot be null");
+        this.moviesView = checkNotNull(moviesView, "moviesView cannot be null");
 
+        moviesView.setPresenter(this);
+    }
 
     @Override
     public void result(int requestCode, int resultCode) {
-
+        //If a favorite movie was successfully added, show a snackbar
+        loadMovies();
     }
 
     @Override
@@ -31,6 +42,6 @@ public class MoviesPresenter implements MoviesContract.Presenter {
 
     @Override
     public void start() {
-
+        loadMovies(false);
     }
 }
